9d3c436f4804d096789c3d5284641cbf1fe41e2c
Gourish Hegde <gourishdattatraya.h@atriauniversity.edu.in>
2026-06-04 10:08:51 +0530

    Remove ecr.tf placeholder comment file

--- infra/ecr.tf
+++
@@ -1,4 +0,0 @@
-# The ECR repo is created by deploy.sh (via the AWS CLI) before the CodeBuild
-# build runs. Terraform does not manage it to avoid chicken-and-egg ordering:
-# the repo must exist before CodeBuild pushes the image and before the ASG
-# launches instances that pull it.
